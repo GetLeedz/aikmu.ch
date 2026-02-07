15d0dad23a46a9b07bfa88cb548fc0e773892b7c
GetLeedz <81183998+GetLeedz@users.noreply.github.com>
2026-02-07 16:52:29 +0100

    Update NavBar.jsx

--- components/navBar/NavBar.jsx
+++ components/navBar/NavBar.jsx
@@ -1,29 +1,22 @@
 // components/navBar/NavBar.jsx
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "../../public/images/getleedz-logo.png";
 
 const NavBar = () => {
   return (
-    // Fixierter Header – schneidet alles darunter sauber ab
     <nav className="fixed inset-x-0 top-0 z-50 overflow-hidden">
-      {/* Top-Banner mit Blur & dunklem Hintergrund */}
       <div className="backdrop-blur-xl bg-[#020617]/80 border-b border-white/5">
-        {/* Logo zentriert */}
         <div className="flex justify-center py-3">
-          <Link href="/" legacyBehavior>
-            <a
-              aria-label="GetLeedz Home"
-              className="relative block w-[200px] h-[60px] sm:w-[240px] sm:h-[72px] lg:w-[260px] lg:h-[80px]"
-            >
+          <Link href="/" aria-label="AiKMU Home">
+            <div className="relative block w-[200px] h-[60px] sm:w-[240px] sm:h-[72px] lg:w-[260px] lg:h-[80px]">
               <Image
-                src={Logo}
-                alt="GetLeedz Logo"
-                layout="fill"
-                objectFit="contain"
+                src="/images/logo-aikmu-transparent_white_transparent.png"
+                alt="AiKMU Logo"
+                fill
+                className="object-contain"
                 priority
               />
-            </a>
+            </div>
           </Link>
         </div>
       </div>
